e4dd3053a42069876b6cf482990feebeedcadb7e
xing-zhi, jiang <a97410985new@gmail.com>
2021-04-08 13:58:26 +0800

    1. update readme.md 2. suppport # micro statement 3. add missing C.g4

--- ExtractKeyword.java
+++ ExtractKeyword.java
@@ -5,14 +5,23 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.io.File;
+import java.util.regex.Pattern;
 
-public class extract_keyword {
+public class ExtractKeyword {
+
+    public static String removeMacroLine(String code) {
+        String regex = "^#.*$";
+        Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
+        String newCode = pattern.matcher(code).replaceAll("");
+        return newCode;
+    }
+
     public static void main(String[] args) throws IOException {
         String filePath = new File("").getAbsolutePath();
         filePath.concat("lex_file.txt");
 
-        CharStream charStream = CharStreams.fromString(
-                String.join("\n", Files.readAllLines(new File("lex_file.txt").toPath(), Charset.forName("utf8"))));
+        String code = String.join("\n", Files.readAllLines(new File("lex_file.txt").toPath(), Charset.forName("utf8")));
+        CharStream charStream = CharStreams.fromString(removeMacroLine(code));
         CLexer lexer = new CLexer(charStream);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         CParser parser = new CParser(tokens);
